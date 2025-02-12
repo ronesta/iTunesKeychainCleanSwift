19e3df2de282c22985cd43f374784acc102219c0
ronesta <gvozzzd123@gmail.com>
2025-02-12 15:32:53 +0300

    First iteration of Clean Swift architecrure

---
+++ iTunesKeychainCleanSwift/iTunesKeychainCleanSwift/Network/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  iTunesKeychainCleanSwift
+//
+//  Created by Ибрагим Габибли on 11.02.2025.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+}
